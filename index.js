be239013bc1d3a380a11d62c1854599060c3844e
Chris Vensko <chrisvensko@gmail.com>
2013-05-05 17:00:38 -0400

    Moved jquery to external javascript file and added the ability to connect/disconnect to the socket.

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var init = function(app) {
 	app.get('/', function (req, res) {
 		res.sendfile(path.resolve(__dirname,'index.html'));
 	});
+	app.get('/jquery.js', function(req, res) {
+		res.sendfile(path.resolve(__dirname, 'lib/jquery.js'));
+	});
 };
 
 module.exports = init;
